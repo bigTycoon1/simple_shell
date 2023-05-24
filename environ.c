725d3246fcc51515939b68dc5a3055802ef67edb
bigTycoon1 <mztatycoon@gmail.com>
2023-05-24 07:52:05 -0700

    0x16. C - Simple Shel

---
+++ environ.c
@@ -0,0 +1,91 @@
+#include "shell.h"
+#include <stdlib.h>
+/**
+ * _myenv - prints the environ variabe
+ * @info: info of the potential arguments
+ * Return: Always 0
+ *
+ */
+int _myenv(info_t *info)
+{
+	print_list_str(info->env);
+	return (0);
+}
+
+/**
+ * _getenv - gets the value of an environ variable
+ * @info: info of a potential arguments.
+ * @name: name of the environ variable
+ *
+ * Return: the value
+ */
+char *_getenv(info_t *info, const char *name)
+{
+	list_t *node = info->env;
+	char *ptr;
+
+	while (node)
+	{
+		ptr = starts_with(node->str, name);
+		if (ptr && *ptr)
+			return (ptr);
+		node = node->next;
+	}
+	return (NULL);
+}
+
+/**
+ * _mysetenv - set or update an environment variable,
+ * @info: info of potential arguments.
+ *
+ *  Return: Always 0
+ */
+int _mysetenv(info_t *info)
+{
+	if (info->argc != 3)
+	{
+		_eputs("Incorrect number of arguements\n");
+		return (1);
+	}
+	if (_setenv(info, info->argv[1], info->argv[2]))
+		return (0);
+	return (1);
+}
+
+/**
+ * _myunsetenv - remove an environment variable from a current process
+ * @info: info potential arguments
+ *
+ *  Return: Always 0
+ */
+int _myunsetenv(info_t *info)
+{
+	int x;
+
+	if (info->argc == 1)
+	{
+		_eputs("Too few arguements.\n");
+		return (1);
+	}
+	for (x = 1; x <= info->argc; x++)
+		_unsetenv(info, info->argv[x]);
+
+	return (0);
+}
+
+/**
+ * populate_env_list - populate the array and then print the environ variables 
+ * @info: info of the potential arguments.
+ *
+ * Return: Always 0
+ */
+int populate_env_list(info_t *info)
+{
+	list_t *node = NULL;
+	size_t x;
+
+	for (x = 0; environ[x]; x++)
+		add_node_end(&node, environ[x], 0);
+	info->env = node;
+	return (0);
+}
---
+++ exits.c
@@ -0,0 +1,76 @@
+#include "shell.h"
+#include <stdlib.h>
+/**
+ **_strncpy - string to copy
+ *@dest: the destination of a string
+ *@src: the source string
+ *@n: the number of characters to be copied
+ *
+ *Return: the concatenated string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int x, y;
+	char *s = dest;
+
+	x = 0;
+	while (src[x] != '\0' && x < n - 1)
+	{
+		dest[x] = src[x];
+		x++;
+	}
+	if (x < n)
+	{
+		y = x;
+		while (y < n)
+		{
+			dest[y] = '\0';
+			y++;
+		}
+	}
+	return (s);
+}
+
+/**
+ **_strncat -  two strings to be concatenate
+ *@dest: the destination string
+ *@src: the source string
+ *@n: the number of bytes to be maximize
+ *
+ *Return: the concatenated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
+}
+
+/**
+ **_strchr - character of a string
+ *@s: the string
+ *@c: the char of the string
+ *Return: (s)
+ */
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
+}
