2c6398c5d6ef6ffe0bb02361721410c35bd2f860
codinBabe <teboola2@gmail.com>
2023-05-26 06:51:26 -0700

    update shell

--- shell.c
+++ shell.c
@@ -1,49 +1,70 @@
 #include "shell.h"
 /**
+ * prompt - display prompt
+ */
+void prompt(void)
+{
+	printf("$ ");
+}
+/**
+ * read_input - This function read input from stdin
+ * Return: input
+ */
+char *read_input()
+{
+	char *input = NULL;
+	size_t bufsize = 0;
+
+	_getline(&input, &bufsize, stdin);
+
+	return (input);
+}
+/**
+ * _execve - This function print env process
+ * @command: input from stdin
+ */
+void _execve(char *command)
+{
+	int stat;
+	pid_t pid = fork();
+
+	if (pid < 0)
+		perror("./shell");
+	else if (pid == 0)
+	{
+		char *args[] = {"/bin/ls", NULL};
+
+		args[1] = command;
+		execvp(args[0], args);
+
+		perror("./shell");
+		exit(EXIT_FAILURE);
+	}
+	else
+		waitpid(pid, &stat, 0);
+}
+/**
  * main - Entry point
- *
- * Return: Always 0
+ * Return: Always zero
  */
 int main(void)
 {
-	char input[BUFF_SIZ];
-	int stat;
-	pid_t pid;
+	char *command;
 
 	while (1)
 	{
-		printf("#cisfun$ ");
-		fflush(stdout);
+		prompt();
 
-		if (fgets(input, sizeof(input), stdin) == NULL)
+		command = read_input();
+
+		if (command == NULL)
 		{
 			printf("\n");
 			break;
 		}
-		input[my_strcspn(input, "\n")] = '\0';
-
-		if (_strcmp(input, "/bin/ls") != 0)
-		{
-			printf("./shell: No such file or directory\n");
-			continue;
-		}
-		pid = fork();
-
-		if (pid < 0)
-		{
-			perror("fork");
-			exit(EXIT_FAILURE);
-		}
-		else if (pid == 0)
-		{
-			if (execl("/bin/ls", "ls", (char *) NULL) == -1)
-			{
-				perror("execl");
-				exit(EXIT_FAILURE);
-			}
-			else
-				waitpid(pid, &stat, 0);
-		}
+		command[my_strcspn(command, "\n")] = '\0';
+		_execve(command);
+		free(command);
 	}
 	return (0);
 }
