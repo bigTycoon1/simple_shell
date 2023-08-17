c615a64bbcfbe3dc73871add4a03a5299b92a9f9
bigTycoon1 <mztatycoon@gmail.com>
2023-08-17 04:40:36 -0700

    simple_shell

--- str_util.c
+++ str_util.c
@@ -2,9 +2,9 @@
 #include "shell.h"
 /**
  * my_strdup - function to duplicate str 
- * *scr - pointer to source ofthe str
+ * *scr: pointer to source ofthe str
  *
- * Return - newly allocated duplicae str
+ * Return: newly allocated duplicae str
  */
 void *my_strdup(const char *src)
 {
@@ -23,4 +23,32 @@ void *my_strdup(const char *src)
 	strcpy(new_str, src);
 
 	return (new_str);
+}
+
+#include <string.h>
+#include "shell.h"
+/**
+ * my_strncmp - function to compare
+ * @s1: first string
+ * @s2: second string 
+ *
+ * Return: 0
+ */
+int my_strncmp(const char *s1, const char *s2, size_t n)
+{
+	char s1, s2;
+	int n, i = 0;
+	for (i = 0; i < n; i++)
+	{
+		if (s1[i] != s2[i])
+	{
+		return (s1[i] - s2[i]);
+	}
+	if (s1[i] == '\0')
+	{
+		break;
+	}
+	
+	}
+	return 0;
 }
